c9689c2092384bd2b39e994a86a437262401136c
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2022-06-30 21:58:19 +0200

    Adding User entity and UserRepository

---
+++ src/main/java/com/greg/banking_app/domain/User.java
@@ -0,0 +1,46 @@
+package com.greg.banking_app.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "USERS")
+public class User {
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "ID_User", unique = true)
+    private Long userId;
+
+    @NotNull
+    @Column(name = "PESEL", unique = true)
+    private String peselNumber;
+
+    @NotNull
+    @Column(name = "FIRSTNAME")
+    private String firstName;
+
+    @NotNull
+    @Column(name = "LASTNAME")
+    private String lastName;
+
+    @NotNull
+    @Column(name = "EMAIL")
+    private String email;
+
+    @NotNull
+    @Column(name = "TELEPHONE")
+    private String telephone;
+
+    @NotNull
+    @Column(name = "IS_ACTIVE")
+    private boolean active;
+}
