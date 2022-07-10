21387c2f1b7cba0da5113a47297099642900f8d8
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2022-07-10 12:48:07 +0200

    Adding InstallmentMapper and constructor to Installment

--- src/main/java/com/greg/banking_app/domain/Installment.java
+++ src/main/java/com/greg/banking_app/domain/Installment.java
@@ -1,5 +1,6 @@
 package com.greg.banking_app.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "INSTALLMENTS")
 public class Installment {
 
