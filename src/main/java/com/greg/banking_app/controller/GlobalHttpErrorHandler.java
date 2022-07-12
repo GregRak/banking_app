3fa4a1456c25b607d4b18a7fc59556a0e76cff06
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2022-07-12 22:55:52 +0200

    Adding GlobalHttpErrorHandler

---
+++ src/main/java/com/greg/banking_app/controller/GlobalHttpErrorHandler.java
@@ -0,0 +1,42 @@
+package com.greg.banking_app.controller;
+
+import com.greg.banking_app.exception.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalHttpErrorHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException exception) {
+        return new ResponseEntity<>("User with given Id doesn't exist", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ResponseEntity<Object> handleAccountNotFoundException(AccountNotFoundException exception) {
+        return new ResponseEntity<>("Account with given Id doesn't exist", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(LoanNotFoundException.class)
+    public ResponseEntity<Object> handleLoanNotFoundException(LoanNotFoundException exception) {
+        return new ResponseEntity<>("Loan with given Id doesn't exist", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(OperationNotFoundException.class)
+    public ResponseEntity<Object> handleOperationNotFoundException(OperationNotFoundException exception) {
+        return new ResponseEntity<>("Operation with given Id doesn't exist", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserAddressNotFoundException.class)
+    public ResponseEntity<Object> handleUserAddressNotFoundException(UserAddressNotFoundException exception) {
+        return new ResponseEntity<>("Address with given Id doesn't exist", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InstallmentNotFoundException.class)
+    public ResponseEntity<Object> handleInstallmentNotFoundException(InstallmentNotFoundException exception) {
+        return new ResponseEntity<>("Installment with given Id doesn't exist", HttpStatus.BAD_REQUEST);
+    }
+}
