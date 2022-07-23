e0055b3749fa735081c18f5c894a658811c83fdd
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2022-07-23 10:45:57 +0200

    Adding NBPController tests

---
+++ src/test/java/com/greg/banking_app/controller/NBPControllerTest.java
@@ -0,0 +1,35 @@
+package com.greg.banking_app.controller;
+
+import com.greg.banking_app.client.NBPClient;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitConfig
+@WebMvcTest(NBPController.class)
+class NBPControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private NBPClient client;
+
+    @Test
+    void shouldGetTableC() throws Exception {
+        //When & Then
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/v1/nbp/tableC")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
