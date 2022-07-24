948dea0f6b33656177ef5d5c6f25ba49aee5c123
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2022-07-24 14:59:41 +0200

    Adding LoanMapper tests and builder to LoanBaseD

--- src/main/java/com/greg/banking_app/dto/loan/LoanBaseDto.java
+++ src/main/java/com/greg/banking_app/dto/loan/LoanBaseDto.java
@@ -21,5 +21,74 @@ public class LoanBaseDto {
     private CurrencySymbol currencySymbol;
     private boolean active;
     private Long accountId;
+
+    public static class LoanBaseDtoBuilder {
+        private Long loanId;
+        private BigDecimal startValue;
+        private BigDecimal currentValue;
+        private BigDecimal interestRate;
+        private LocalDate startDate;
+        private LocalDate endDate;
+        private int period;
+        private CurrencySymbol currencySymbol;
+        private boolean active;
+        private Long accountId;
+
+        public LoanBaseDto.LoanBaseDtoBuilder loanId(Long loanId) {
+            this.loanId = loanId;
+            return this;
+        }
+
+        public LoanBaseDto.LoanBaseDtoBuilder startValue(BigDecimal startValue) {
+            this.startValue = startValue;
+            return this;
+        }
+
+        public LoanBaseDto.LoanBaseDtoBuilder currentValue(BigDecimal currentValue) {
+            this.currentValue = currentValue;
+            return this;
+        }
+
+        public LoanBaseDto.LoanBaseDtoBuilder interestRate(BigDecimal interestRate) {
+            this.interestRate = interestRate;
+            return this;
+        }
+
+        public LoanBaseDto.LoanBaseDtoBuilder startDate(LocalDate startDate) {
+            this.startDate = startDate;
+            return this;
+        }
+
+        public LoanBaseDto.LoanBaseDtoBuilder endDate(LocalDate endDate) {
+            this.endDate = endDate;
+            return this;
+        }
+
+        public LoanBaseDto.LoanBaseDtoBuilder period(int period) {
+            this.period = period;
+            return this;
+        }
+
+        public LoanBaseDto.LoanBaseDtoBuilder currencySymbol(CurrencySymbol currencySymbol) {
+            this.currencySymbol = currencySymbol;
+            return this;
+        }
+
+        public LoanBaseDto.LoanBaseDtoBuilder active(boolean active) {
+            this.active = active;
+            return this;
+        }
+
+        public LoanBaseDto.LoanBaseDtoBuilder accountId(Long accountId) {
+            this.accountId = accountId;
+            return this;
+        }
+
+        public LoanBaseDto build() {
+            return new LoanBaseDto(
+                    loanId, startValue, currentValue, interestRate, startDate, endDate, period, currencySymbol, active, accountId
+            );
+        }
+    }
 }
 
